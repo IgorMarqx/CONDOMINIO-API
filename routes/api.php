e619917c24df1cd33e875a16a458b0fc077b3347
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-11 15:28:09 -0300

    feat; create routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,44 @@
 <?php
 
+use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\cond\BilletController;
+use App\Http\Controllers\cond\DocController;
+use App\Http\Controllers\cond\FoundAndLostController;
+use App\Http\Controllers\cond\WallController;
+use App\Http\Controllers\cond\WarningController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping',  function () {
     return  ['pong' =>  true];
+});
+
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    // Mural de avisos
+    Route::get('/walls', [WallController::class, 'getAll']);
+    Route::post('/wall/{id}/like', [WallController::class, 'like']);
+
+    // Documentos
+    Route::get('/docs', [DocController::class, 'getAll']);
+
+    // Ocorrencias
+    Route::get('/warnings', [WarningController::class, 'getMyWarnings']);
+    Route::post('/warning', [WarningController::class, 'setWarning']);
+    Route::post('/warning/file', [WarningController::class, 'addWarningFile']);
+
+    // Boletos
+    Route::get('/billets', [BilletController::class, 'getAll']);
+
+    // Achados e Perdidos
+    Route::get('/foundandlost', [FoundAndLostController::class, 'getAll']);
+    Route::post('/foundandlost', [FoundAndLostController::class, 'insert']);
+    Route::put('/foundandlost/{id}', [FoundAndLostController::class, 'update']);
 });
