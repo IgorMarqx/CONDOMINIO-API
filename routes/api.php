f272701e89ff3f7d446b8e81531d340a66e3665b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-11 17:00:55 -0300

    feat: create all routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\auth\AuthController;
 use App\Http\Controllers\cond\BilletController;
 use App\Http\Controllers\cond\DocController;
 use App\Http\Controllers\cond\FoundAndLostController;
+use App\Http\Controllers\cond\ReservationController;
 use App\Http\Controllers\cond\WallController;
 use App\Http\Controllers\cond\WarningController;
+use App\Http\Controllers\unit\UnitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -41,4 +43,24 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/foundandlost', [FoundAndLostController::class, 'getAll']);
     Route::post('/foundandlost', [FoundAndLostController::class, 'insert']);
     Route::put('/foundandlost/{id}', [FoundAndLostController::class, 'update']);
+
+    // Unidade
+    Route::get('/unit/{id}', [UnitController::class, 'getInfo']);
+    Route::post('/unit/{id}/addperson', [UnitController::class, 'addPerson']);
+    Route::post('/unit/{id}/addvehicle', [UnitController::class, 'addvehicle']);
+    Route::post('/unit/{id}/addpet', [UnitController::class, 'addpet']);
+
+    Route::delete('/unit/{id}/removeperson', [UnitController::class, 'removeperson']);
+    Route::delete('/unit/{id}/removevehicle', [UnitController::class, 'removevehicle']);
+    Route::delete('/unit/{id}/removepet', [UnitController::class, 'removepet']);
+
+    // Reservas
+    Route::get('/reservations', [ReservationController::class, 'getReservations']);
+    Route::post('/reservation/{id}', [ReservationController::class, 'setReservation']);
+
+    Route::get('/reservation/{id}/disableddates', [ReservationController::class, 'getDisabledDates']);
+    Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes']);
+
+    Route::get('/my/reservations', [ReservationController::class, 'getMyReservations']);
+    Route::delete('/myreservation/{id}', [ReservationController::class, 'delMyReservation']);
 });
