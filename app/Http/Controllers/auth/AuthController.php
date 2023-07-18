aa82822893a0f7a2b13d18106a96d1c9f97505ac
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-18 13:57:39 -0300

    feat: create login, logout and validateToken

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -74,4 +74,70 @@ class AuthController extends Controller
 
         return $array;
     }
+
+    public function login(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'cpf' => ['required', 'digits:11'],
+            'password' => ['required']
+        ]);
+
+        if (!$validator->fails()) {
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            $token = Auth::attempt([
+                'cpf' => $cpf,
+                'password' => $password,
+            ]);
+
+            if (!$token) {
+                $array['error'] = 'Cpf ou senha inválidos.';
+                return $array;
+            }
+
+            $array['token'] = $token;
+
+            $user = Auth::user();
+            $array['user'] = $user;
+
+            $properties = Unit::select(['id', 'name'])
+                ->where('id_owner', $user['id'])
+                ->get();
+
+            $array['user']['properties'] = $properties;
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
+
+    public function validateToken()
+    {
+        $array = ['error' => ''];
+
+        $user = Auth::user();
+        $array['user'] = $user;
+
+        $properties = Unit::select(['id', 'name'])
+            ->where('id_owner', $user['id'])
+            ->get();
+
+        $array['user']['properties'] = $properties;
+
+        return $array;
+    }
+
+    public function logout()
+    {
+        $array = ['error' => ''];
+
+        Auth()->logout();
+
+        return $array;
+    }
 }
