31bf616b9132ddb1f5144693c873a5b9f932ae97
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-19 14:01:47 -0300

    feat: docController function getAll

--- app/Http/Controllers/cond/DocController.php
+++ app/Http/Controllers/cond/DocController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\cond;
 
 use App\Http\Controllers\Controller;
+use App\Models\Doc;
 use Illuminate\Http\Request;
 
 class DocController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $docs = Doc::all();
+
+        foreach ($docs as $docKey => $docValue) {
+            $docs[$docKey]['fileurl'] = asset('storage/' . $docValue['fileurl']);
+        }
+
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
