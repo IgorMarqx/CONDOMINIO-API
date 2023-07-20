ba0db1d4903e7d69faa3406ac6abe12fd5d68580
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-20 15:10:28 -0300

    feat: create implementation warningController

--- app/Http/Controllers/cond/WarningController.php
+++ app/Http/Controllers/cond/WarningController.php
@@ -7,6 +7,8 @@ use App\Models\Unit;
 use App\Models\Warning;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class WarningController extends Controller
 {
@@ -24,7 +26,6 @@ class WarningController extends Controller
                 ->count();
 
             if ($unit > 0) {
-
                 $warnings = Warning::where('id_unit', $property)
                     ->orderBy('created_at', 'DESC')
                     ->orderBy('id', 'DESC')
@@ -52,6 +53,68 @@ class WarningController extends Controller
             $array['error'] = 'Insira uma propriedade.';
         }
 
+
+        return $array;
+    }
+
+    public function addWarningFile(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'photo' => ['required', 'file', 'mimes:jpg,png'],
+        ]);
+
+        if (!$validator->fails()) {
+            $file = $request->file('photo')->store('public');
+
+            $array['photo'] = asset(Storage::url($file));
+        } else {
+            $array['error'] =   $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
+
+    public function setWarning(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(),  [
+            'title' => ['required'],
+            'property' => ['required'],
+        ]);
+
+        if (!$validator->fails()) {
+            $title = $request->input('title');
+            $property = $request->input('property');
+            $list = $request->input('list');
+
+            $newWarning = new Warning();
+            $newWarning->id_unit = $property;
+            $newWarning->title = $title;
+            $newWarning->status = 'IN_REVIEW';
+            $newWarning->created_at = date('Y-m-d');
+
+            if ($list && is_array($list)) {
+                $photos = [];
+
+                foreach ($list as $listItem) {
+                    $url = explode('/', $listItem);
+                    $photos[] = end($url);
+                }
+
+                $newWarning->photos = implode(',', $photos);
+            }else{
+                $newWarning->photos = ' ';
+            }
+
+            $newWarning->save();
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
 
         return $array;
     }
