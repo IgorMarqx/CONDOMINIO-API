7d4ba2b79234ac8b38dd87505ab47bc34bcb2537
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-24 16:58:18 -0300

    feat: function setReservation

--- app/Http/Controllers/cond/ReservationController.php
+++ app/Http/Controllers/cond/ReservationController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\cond;
 
 use App\Http\Controllers\Controller;
 use App\Models\Area;
+use App\Models\AreaDisabledDay;
 use App\Models\Reservation;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -80,11 +82,71 @@ class ReservationController extends Controller
         $array = ['error' => ''];
 
         $validator = Validator::make($request->all(), [
-            'id_area' => ['required'],
-            'reservation_date' => ['required', 'date'],
+            'date' => ['required', 'date_format:Y-m-d'],
+            'time' => ['required', 'date_format:H:i:s'],
+            'property' => ['required'],
         ]);
 
         if (!$validator->fails()) {
+            $date = $request->input('date');
+            $time = $request->input('time');
+            $property = $request->input('property');
+
+            $unit = Unit::find($property);
+            $area = Area::find($id);
+
+            if ($unit && $area) {
+                $can = true;
+
+                $weekDay = date('w', strtotime($date));
+
+                $allowedDays = explode(',', $area['days']);
+                if (!in_array($weekDay, $allowedDays)) {
+                    $can = false;
+                } else {
+                    $start = strtotime($area['start_time']);
+                    $end = strtotime('-1 hour', strtotime($area['end_time']));
+                    $revtime = strtotime($time);
+
+                    if ($revtime < $start || $revtime > $end) {
+                        $can = false;
+                    }
+                }
+
+                // Verificar se está dentro dos DisabledDays
+                $existingDisabledDay = AreaDisabledDay::where('id_area', $id)
+                    ->where('day', $date)
+                    ->count();
+
+                if ($existingDisabledDay > 0) {
+                    $can = false;
+                }
+
+                // Verificar se não existe outra reserva no mesmo dia/hora
+                $existingReservations = Reservation::where('id_area', $id)
+                    ->where('reservation_date', $date . ' ' . $time)
+                    ->count();
+
+                if ($existingReservations > 0) {
+                    $can = false;
+                }
+
+                if ($can) {
+                    $newReservation = new Reservation();
+
+                    $newReservation->id_unit = $property;
+                    $newReservation->id_area = $id;
+                    $newReservation->reservation_date = $date . ' ' . $time;
+
+                    $newReservation->save();
+                } else {
+                    $array['error'] = 'Reserva não permitida neste dia/horário.';
+                    return $array;
+                }
+            } else {
+                $array['error'] = 'Dados incorretos.';
+                return $array;
+            }
         } else {
             $array['error'] = $validator->errors()->first();
             return $array;
